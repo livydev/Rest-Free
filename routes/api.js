5f05f7aae3d48dbf7d553a4343564e8c464c7d17
LivyNabilaDev <104191579+livydev@users.noreply.github.com>
2022-05-03 03:21:54 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ var zahirr = db.get("zahirr");
 	console.log('')  
 }
  
-var creator = "Nathan"
+var creator = "LoliChan"
 var secure = require('ssl-express-www');
 var cors = require('cors');
 var fetch = require('node-fetch');
@@ -175,7 +175,7 @@ Akhir Pesan Error
 
 router.use(favicon(__path + "/views/favicon.ico"));
 
-const listkey = ["ISISENDIRI"];
+const listkey = ["Livyapi"];
 
 router.post("/apikey", async (req, res, next) => {
   const key = req.query.key;
